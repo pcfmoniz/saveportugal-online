3a1250e2ee97c9ba1ff4a33c8224321bb2d417c3
pcfmoniz <paulo@mnz.pt>
2024-08-15 20:24:29 +0100

    meas

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Fim à exploração de polvos na Grã Canária",
+        "excerpt":"Apelamos a que envies este email modelo para a Embaixada de Espanha em Portugal, como demonstração ao Governo de Espanha que a criação, exploração e abate de polvos em cativeiro é uma actividade sem sentido e atentatória do bem-estar animal, em especial desta espécie altamente senciente. Podes adaptar o texto...","categories": [],
+        "tags": [],
+        "url": "/agir/espanha/",
+        "teaser": "/assets/images/polvos_gran_canaria-teaser.webp"
+      },{
+        "title": "Acção imediata contra montarias na Serra da Lousã - 24 e 29 fevereiro 2024",
+        "excerpt":"ASSUNTO: Massacres na Serra da Lousã DESTINATÁRIOS: sig@icnf.pt, dgrca@icnf.pt, geral@icnf.pt, fatima.reis@icnf.pt, secretariado.cd@icnf.pt, DRCNF.Centro@icnf.pt, CC: gap@cm-lousa.pt, geral@cm-lousa.pt, geral@baldiosvilarinho-lsa.pt, aurelio.goncalves@baldiosvilarinho-lsa.pt, ct.cbr.dlsa.plsa@gnr.pt, ct.cbr@gnr.pt, sepna@gnr.pt CORPO DA MENSAGEM: Ex.mos e Ex.mas Srs. e Srs.ª do Instituto da Conservação da Natureza e das Florestas, Tive conhecimento que, no passado dia 13 de Fevereiro de 2024,...","categories": [],
+        "tags": [],
+        "url": "/agir/icnf/",
+        "teaser": "/assets/images/icnf-lousa-teaser.webp"
+      },{
+        "title": "Nota de Repúdio - Montaria Serra da Lousã",
+        "excerpt":"ASSUNTO: Nota de Repúdio - Montaria Serra da Lousã - 06/jan/24 DESTINATÁRIOS: Executivo da Câmara Municipal da Lousã CORPO DA MENSAGEM: Ex.mo Sr. Presidente da Câmara da Lousã, Tive conhecimento que, no próximo dia 6 de janeiro de 2024, estão a planear uma montaria ao javali e veado, na Serra...","categories": [],
+        "tags": [],
+        "url": "/agir/montaria/",
+        "teaser": "/assets/images/montaria-lousa.webp"
+      },{
+        "title": "Pela Abolição da Exportação de Animais Vivos por via marítima para países terceiros",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/agir/patav/",
+        "teaser": "/assets/images/patav-teaser.webp"
+      },{
+        "title": "Travar o massacre de pombos no Cartaxo",
+        "excerpt":"envia a mensagem ASSUNTO: Travar o massacre de pombos QUE CONTINUA no Cartaxo DESTINATÁRIOS: :correio@cm-cartaxo.pt, gap@cm-cartaxo.pt, geral@jf-cartaxoevaledapinta.pt, sepna@gnr.pt, secretariado.cd@icnf.pt, geral@icnf.pt, DRCNF.LVT@icnf.pt, icnf@icnf.pt, geral@dgav.pt, atendimento.davr@dgav.pt, jheitor@cm-cartaxo.pt, preis@cm-cartaxo.pt, moliveira@cm-cartaxo.pt, cdsantarem@psp.pt, defesanimal@psp.pt CORPO DA MENSAGEM: Exmos. senhores Exmas. senhoras Com a exceção de um e-mail assinado pelo Chefe da Divisão Técnica Ambiental, da Guarda...","categories": [],
+        "tags": [],
+        "url": "/agir/pombos/",
+        "teaser": "/assets/images/pombos-v2-cartaxo-teaser.webp"
+      },{
+        "title": "O 4º R - Direito ao Realojamento",
+        "excerpt":"O Animal Save &amp; Care Portugal e os Biotérios da Universidade de Coimbra estão a colaborar para estabelecer um protocolo que vise a possibilidade de resgate de animais usados em experiências laboratoriais (pequenos roedores). Estando cientes de que o processo de adoção destes animais está contemplado na lei de utilização...","categories": [],
+        "tags": [],
+        "url": "/agir/realojamento/",
+        "teaser": "/assets/images/realojamento-teaser.webp"
+      },{
+        "title": "SAGRES, não patrocines recintos que acolhem touradas",
+        "excerpt":"Esta é uma acção da Plataforma Basta de Touradas à qual nos juntamos. ASSUNTO: Sagres, não patrocines recintos que acolhem touradas. DESTINATÁRIOS: servico.cliente@centralcervejas.pt CORPO DA MENSAGEM: Exmo. Senhor Presidente da Sociedade Central de Cervejas, Tomei conhecimento que a SAGRES passou a patrocinar a sala de espetáculos do Campo Pequeno, recinto...","categories": [],
+        "tags": [],
+        "url": "/agir/sagrestourada/",
+        "teaser": "/assets/images/sagres-tourada-teaser.webp"
+      },{
+        "title": "Sintra mais bonita e romântica sem crueldade",
+        "excerpt":"envia a mensagem ASSUNTO: Sintra mais Bonita e Romântica sem Crueldade DESTINATÁRIO: municipe@cm-sintra.pt, gsvm@cm-sintra.pt CORPO DA MENSAGEM: Ex.mo Senhor Presidente da Câmara Municipal de Sintra Dr. Basílio Horta, Ex.mas e Ex.mos Senhoras e Senhores Vereadores do Executivo Camarário de Sintra, Estas imagens, tiradas a 5 de Agosto de 2023 no...","categories": [],
+        "tags": [],
+        "url": "/agir/sintra/",
+        "teaser": "/assets/images/cavalos-sintra-teaser.webp"
+      },{
+        "title": "A tortura não tem lugar na televisão pública",
+        "excerpt":"Esta é uma acção da Plataforma Basta de Touradas à qual nos juntamos. Os aficcionados estão a pedir à Provedora do Telespectador que a RTP reverta a sua decisão, do passado, de não exibir touradas na televisão pública. Se queres que o panorama actual não se altere, envia também uma...","categories": [],
+        "tags": [],
+        "url": "/agir/touradas/",
+        "teaser": "/assets/images/touradas-rtp-teaser.webp"
+      },]
